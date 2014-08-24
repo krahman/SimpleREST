e9dcf707067e5dce1e989837b682c26bf4911201
khal <khal.rahman@gmail.com>
2014-08-24 18:29:38 +0800

    installed hogan and uninstall jade

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 
 app.use(favicon());
 app.use(logger('dev'));
